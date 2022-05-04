ab86e8ec42afa5cf783d6fb772126b2ca7c6433d
yoon92kr <yoon92_kr@naver.com>
2022-05-04 09:11:16 +0900

    2558_A+B - 2

---
+++ Baekjoon/BAEKJOON_2558.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// public class Main {
+public class BAEKJOON_2558 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+
+		bw.write(Integer.toString(a + b));
+
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+}
