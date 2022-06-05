67587aef00e3a286f52271b550a95bd103574c3d
yoon92kr <yoon92_kr@naver.com>
2022-06-05 13:26:45 +0900

    10172_개

---
+++ Baekjoon/BAEKJOON_10172.java
@@ -0,0 +1,13 @@
+// public class Main {
+public class BAEKJOON_10172 {
+	
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+
+	}
+}
