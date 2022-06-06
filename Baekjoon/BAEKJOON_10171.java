3203a1a463c448e56abd099a9241d3970c91a0e9
yoon92kr <yoon92_kr@naver.com>
2022-06-06 10:13:01 +0900

    9498_시험 성적

---
+++ Baekjoon/BAEKJOON_10171.java
@@ -0,0 +1,12 @@
+// public class Main {
+public class BAEKJOON_10171 {
+	
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+}
