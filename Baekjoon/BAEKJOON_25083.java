771b1001c9e198b322601bf8c678eda867473d60
yoon92kr <yoon92_kr@naver.com>
2022-11-17 17:29:41 +0900

    25083_새싹

---
+++ Baekjoon/BAEKJOON_25083.java
@@ -0,0 +1,13 @@
+// public class Main {
+public class BAEKJOON_25083 {
+	public static void main(String[] args) throws Exception {
+
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+
+	}
+}
