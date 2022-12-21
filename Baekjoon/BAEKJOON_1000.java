42ec08ea89f6121c49c23fc9020a2942ed482124
yoon92kr <yoon92_kr@naver.com>
2022-12-22 08:43:38 +0900

    1000_A+B

---
+++ Baekjoon/BAEKJOON_1000.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// public class Main {
+
+public class BAEKJOON_1000 {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String param = br.readLine();
+
+		String[] splitParam = param.split(" ");
+
+		System.out.println(Integer.parseInt(splitParam[0]) + Integer.parseInt(splitParam[1]));
+		br.close();
+
+	}
+}
