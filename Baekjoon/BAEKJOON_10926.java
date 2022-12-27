582886419387d01f87760d4400ee5caa1b9459a2
yoon92kr <yoon92_kr@naver.com>
2022-12-28 08:48:53 +0900

    10926_??!

---
+++ Baekjoon/BAEKJOON_10926.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// public class Main {
+public class BAEKJOON_10926 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String param = br.readLine();
+
+		System.out.println(param + "??!");
+
+		br.close();
+
+	}
+}
