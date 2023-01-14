76956b5b9fb72beb161a9fde58a8477926af0a7f
yoon92kr <yoon92_kr@naver.com>
2023-01-14 12:53:26 +0900

    최솟값 만들기

---
+++ Programmers/Solution_65.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 문제 : 최솟값 만들기
+// 결과 : 성공
+// Score : 100
+public class Solution_65 {
+
+	public int solution(int[] A, int[] B) {
+		return multiplyArray(A, B, A.length);
+	}
+
+	int multiplyArray(int[] A, int[] B, int len) {
+		int result = 0;
+		Arrays.sort(A);
+		Integer[] tmpB = Arrays.stream(B).boxed().toArray(Integer[]::new);
+		Arrays.sort(tmpB, Comparator.reverseOrder());
+		for (int i = 0; i < len; i++) {
+			result += A[i] * tmpB[i];
+		}
+		return result;
+	}
+
+}
