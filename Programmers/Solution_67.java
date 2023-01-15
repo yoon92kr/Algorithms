3b4b8ca12a325d471d2780426e7c5abe240cbcc4
yoon92kr <yoon92_kr@naver.com>
2023-01-15 22:45:26 +0900

    피보나치 수

---
+++ Programmers/Solution_67.java
@@ -0,0 +1,22 @@
+// 문제 : 피보나치 수
+// 결과 : 성공
+// Score : 100
+public class Solution_67 {
+
+	public int solution(int param) {
+		return param < 2 ? 1 : fibonacci(param) % 1234567;
+	}
+
+	int fibonacci(int param) {
+		int[] result = new int[param];
+		result[0] = 0;
+		result[1] = 1;
+
+		for (int i = 2; i < param; i++) {
+			result[i] = result[i - 1] % 1234567 + result[i - 2] % 1234567;
+		}
+
+		return result[param - 1] + result[param - 2];
+	}
+
+}
