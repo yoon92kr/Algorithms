1229f1730f3aaf6df3c2d1b7895ba1d0e5622aed
yoon92kr <yoon92_kr@naver.com>
2023-01-29 16:03:42 +0900

    두 수의 나눗셈

---
+++ Programmers/Solution_82.java
@@ -0,0 +1,11 @@
+// 문제 : 두 수의 나눗셈
+// 결과 : 성공
+// Score : 100
+public class Solution_82 {
+
+	public int solution(int num1, int num2) {
+
+		return (int) ((Double.valueOf(num1) / num2) * 1000);
+	}
+
+}
