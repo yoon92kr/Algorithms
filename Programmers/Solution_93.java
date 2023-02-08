18695e1b0398d24d1bb580700d265abbf19b61df
yoon92kr <yoon92_kr@naver.com>
2023-02-09 08:37:17 +0900

    특정 문자 제거하기

---
+++ Programmers/Solution_93.java
@@ -0,0 +1,12 @@
+// 문제 : 특정 문자 제거하기
+// 결과 : 성공
+// Score : 100
+public class Solution_93 {
+
+	public String solution(String param, String target) {
+		
+		return param.replace(target, "");
+		
+	}
+
+}
