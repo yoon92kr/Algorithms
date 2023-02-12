651a487711d2c9f6f38035c0449c4f46fab59848
yoon92kr <yoon92_kr@naver.com>
2023-02-12 13:48:47 +0900

    피자 나눠 먹기 (3)

---
+++ Programmers/Solution_98.java
@@ -0,0 +1,12 @@
+// 문제 : 피자 나눠 먹기 (3)
+// 결과 : 성공
+// Score : 100
+public class Solution_98 {
+
+	public int solution(int slice, int n) {
+		
+		return (int) Math.ceil((double) n / slice);
+		
+	}
+
+}
