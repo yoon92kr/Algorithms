b0c53d77aed492fd93bad3df2dcc05e5579c1794
yoon92kr <yoon92_kr@naver.com>
2023-02-12 13:48:30 +0900

    아이스 아메리카노

---
+++ Programmers/Solution_97.java
@@ -0,0 +1,12 @@
+// 문제 : 아이스 아메리카노
+// 결과 : 성공
+// Score : 100
+public class Solution_97 {
+
+	public int[] solution(int param) {
+		
+		return new int[] { param / 5500, param % 5500 };
+		
+	}
+
+}
