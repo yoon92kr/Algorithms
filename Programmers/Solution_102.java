a3b7c64972a9ec117d4ab392520c7574e8385f5a
yoon92kr <yoon92_kr@naver.com>
2023-02-15 08:55:22 +0900

    점의 위치 구하기

---
+++ Programmers/Solution_102.java
@@ -0,0 +1,12 @@
+// 문제 : 점의 위치 구하기
+// 결과 : 성공
+// Score : 100
+public class Solution_102 {
+
+	public int solution(String str1, String str2) {
+		
+		return str1.contains(str2) ? 1 : 2;
+		
+	}
+
+}
