05a80c3a3586da8e51ffc0a5d989eaa7e57bdbd2
yoon92kr <yoon92_kr@naver.com>
2023-03-29 08:44:34 +0900

    삼각형의 완성조건 (2)

---
+++ Programmers/Solution_157.java
@@ -0,0 +1,12 @@
+// 문제 : 삼각형의 완성조건 (2)
+// 결과 : 성공
+// Score : 100
+public class Solution_157 {
+
+	public int solution(int[] param) {
+		
+		return Math.min(param[0], param[1]) * 2 - 1;
+		
+	}
+
+}
