da6fe233a3680b7fc7b8b285290580da6ddb2946
yoon92kr <yoon92_kr@naver.com>
2023-04-01 16:01:18 +0900

    OX퀴즈

---
+++ Programmers/Solution_161.java
@@ -0,0 +1,22 @@
+// 문제 : OX퀴즈
+// 결과 : 성공
+// Score : 100
+public class Solution_161 {
+
+	public String[] solution(String[] quiz) {
+		String[] answer = new String[quiz.length];
+
+		for (int i = 0; i < quiz.length; i++) {
+			String[] splitQA = quiz[i].split(" = ");
+			String operator = splitQA[0].indexOf(" + ") != -1 ? " \\+ " : " - ";
+			String[] splitQ = splitQA[0].split(operator);
+			int x = Integer.parseInt(splitQ[0]);
+			int y = Integer.parseInt(splitQ[1]);
+			int z = Integer.parseInt(splitQA[1]);
+			answer[i] = (operator == " \\+ " ? x + y : x - y) == z ? "O" : "X";
+		}
+
+		return answer;
+	}
+
+}
