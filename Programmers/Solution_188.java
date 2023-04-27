5bf707226a88b8d8be120294217dd37c381da4a7
yoon92kr <yoon92_kr@naver.com>
2023-04-27 13:44:14 +0900

    d

--- Programmers/Solution_188.java
+++ Programmers/Solution_188.java
@@ -1,24 +1,10 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
 // 문제 : a와 b 출력하기
 // 결과 : 성공
 // Score : 100
 public class Solution_188 {
 	public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] params = br.readLine().split(" ");
-		bw.write("a = " + params[0] + "\n");
-		bw.write("b = " + params[1]);
-
-		bw.flush();
-		bw.close();
-		br.close();
+		System.out.println(String.format("%s 정보없음(%s", "AS유료화진단", "C"));
 
 	}
 }
