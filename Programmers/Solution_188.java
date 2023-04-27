e46b185761730f1f1f0599412034496fe489e8c7
yoon92kr <yoon92_kr@naver.com>
2023-04-27 13:48:17 +0900

    문자열 반복해서 출력하기

--- Programmers/Solution_188.java
+++ Programmers/Solution_188.java
@@ -1,10 +1,31 @@
-// 문제 : a와 b 출력하기
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// 문제 : 문자열 반복해서 출력하기
 // 결과 : 성공
 // Score : 100
 public class Solution_188 {
 	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		System.out.println(String.format("%s 정보없음(%s", "AS유료화진단", "C"));
+		String[] params = br.readLine().split(" ");
 
+		String str = params[0];
+		int retryCnt = Integer.parseInt(params[1]);
+		String result = "";
+
+		for (int i = 0; i < retryCnt; i++) {
+			result += str;
+		}
+
+		bw.write(result);
+		bw.flush();
+
+		bw.close();
+		br.close();
 	}
+
 }
