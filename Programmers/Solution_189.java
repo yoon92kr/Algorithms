b37662a413ed896ae5a7819d4154262d2f2d6404
yoon92kr <yoon92_kr@naver.com>
2023-04-29 21:05:51 +0900

    달리기 경주

--- Programmers/Solution_189.java
+++ Programmers/Solution_189.java
@@ -1,23 +1,45 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 // 문제 : 달리기 경주
 // 결과 : 성공
-// Score : 68
+// Score : 100
 public class Solution_189 {
 
 	public String[] solution(String[] players, String[] callings) {
-		ArrayList<String> arrPlayers = new ArrayList<String>(Arrays.asList(players));
-		int runCnt = callings.length;
-		int index = 0;
+		String[] answer = new String[players.length];
 
-		for (int i = 0; i < runCnt; i++) {
-			index = arrPlayers.indexOf(callings[i]);
-			arrPlayers.set(index, arrPlayers.get(index - 1));
-			arrPlayers.set(index - 1, callings[i]);
+		Map<Integer, String> map = new TreeMap<>();
+		Map<String, Integer> map2 = new HashMap<>();
+
+		for (int i = 0; i < players.length; i++) {
+			map.put(i + 1, players[i]);
+			map2.put(players[i], i + 1);
 		}
 
-		return arrPlayers.toArray(new String[arrPlayers.size()]);
+		for (int i = 0; i < callings.length; i++) {
+
+			String call = callings[i];
+
+			int idx = map2.get(call);
+
+			String a = map.get(idx - 1);
+
+			map2.put(call, idx - 1);
+			map2.put(a, idx);
+
+			map.put(idx - 1, call);
+			map.put(idx, a);
+
+		}
+
+		int idx = 0;
+		for (int key : map.keySet()) {
+			answer[idx++] = map.get(key);
+		}
+
+		return answer;
 	}
 
 }
