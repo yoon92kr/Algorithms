3733a4cd3b13ff5386585cb6db5bce25d65b0f54
yoon92kr <yoon92_kr@naver.com>
2023-05-03 22:55:28 +0900

    대충 만든 자판

---
+++ Programmers/Solution_192.java
@@ -0,0 +1,11 @@
+// 문제 : 대충 만든 자판
+// 결과 : 성공
+// Score : 100
+public class Solution_192 {
+
+	public int[] solution(String[] keymap, String[] targets) {
+		int[] answer = {};
+		return answer;
+	}
+
+}
