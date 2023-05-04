3d60b51f46ef2fad54f0231c25dc271cdddf9658
yoon92kr <yoon92_kr@naver.com>
2023-05-04 19:03:14 +0900

    크기가 작은 부분문자열

--- Programmers/Solution_192.java
+++ Programmers/Solution_192.java
@@ -1,11 +1,15 @@
-// 문제 : 대충 만든 자판
+// 문제 : 크기가 작은 부분문자열
 // 결과 : 성공
 // Score : 100
 public class Solution_192 {
 
-	public int[] solution(String[] keymap, String[] targets) {
-		int[] answer = {};
-		return answer;
+	public int solution(String baseInt, String targetInt) {
+
+		return 1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("192.168.0.1".indexOf("192.168."));
 	}
 
 }
