f2e03cff0ecc5416d2280a45e4e8e73b807ff375
yoon92kr <yoon92_kr@naver.com>
2023-05-07 16:55:11 +0900

    바탕화면 정리

--- Programmers/Solution_193.java
+++ Programmers/Solution_193.java
@@ -3,18 +3,28 @@
 // Score : 100
 public class Solution_193 {
 
-	public static int solution(String t, String p) {
-		int len = p.length();
-		long num = Long.parseLong(p);
-		int answer = 0;
-
-		for (int i = 0; i <= t.length() - len; i++) {
-			if (Long.parseLong(t.substring(i, i + len)) <= num) {
-				answer++;
+	public int[] solution(String[] wallpaper) {
+		int[] answer = {};
+		answer = new int[4];
+		answer[0] = wallpaper.length + 1;
+		answer[1] = wallpaper.length + 1;
+		int x = 0, y = 0;
+		for (int i = 0; i < wallpaper.length; i++) {
+			if (wallpaper[i].contains("#")) {
+				x = i;
+				y = wallpaper[i].indexOf('#');
+				answer[0] = Math.min(answer[0], x);
+				answer[1] = Math.min(answer[1], y);
+				answer[2] = Math.max(answer[2], x + 1);
+				answer[3] = Math.max(answer[3], y + 1);
+				wallpaper[i] = wallpaper[i].replaceFirst("#", ".");
+				i--;
 			}
 		}
-
+		if (answer[0] == wallpaper.length + 1 && answer[1] == wallpaper.length + 1) {
+			answer[0] = 0;
+			answer[1] = 0;
+		}
 		return answer;
 	}
-
 }
