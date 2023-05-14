3be2cb9427b1105a0ec85ca3a1b7402e630a84d8
yoon92kr <yoon92_kr@naver.com>
2023-05-14 19:51:38 +0900

    n 번째 원소까지

---
+++ Programmers/Solution_199.java
@@ -0,0 +1,17 @@
+// 문제 : n 번째 원소까지
+// 결과 : 성공
+// Score : 100
+
+public class Solution_199 {
+
+	public int[] solution(int[] num_list, int length) {
+		int[] result = new int[length];
+
+		for (int i = 0; i < length; i++) {
+			result[i] = num_list[i];
+		}
+
+		return result;
+	}
+
+}
