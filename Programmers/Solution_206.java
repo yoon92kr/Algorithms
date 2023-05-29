065925a26e03260c0a04370e1146a76c7fda7c19
yoon92kr <yoon92_kr@naver.com>
2023-05-29 20:22:10 +0900

    공백으로 구분하기 1

---
+++ Programmers/Solution_206.java
@@ -0,0 +1,12 @@
+// 문제 : 공백으로 구분하기 1
+// 결과 : 성공
+// Score : 100
+
+public class Solution_206 {
+
+    public String[] solution(String param) {
+            
+        return param.split(" ");
+
+    }
+}
