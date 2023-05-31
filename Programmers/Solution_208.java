8a40bbf49b9579dc9a5d31a463ca1008e432991c
yoon92kr <yoon92_kr@naver.com>
2023-06-01 08:57:25 +0900

    배열 만들기 1

--- Programmers/Solution_208.java
+++ Programmers/Solution_208.java
@@ -5,13 +5,13 @@
 public class Solution_208 {
 
     public int[] solution(int limit, int base) {
-        int[] answer = new int[limit / base];
+        int[] result = new int[limit / base];
         
-        for(int i = 0 , j = base ; j <= limit ; j*=2, i++) {
-            answer[i] = j;
+        for(int i = 0 , j = base ; j <= limit ; j+=base, i++) {
+            result[i] = j;
         }
 
-        return answer;
+        return result;
     }
 
 }
