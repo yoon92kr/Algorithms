a55a96bfed893612e22ea512380bf82f09f606f5
yoon92kr <yoon92_kr@naver.com>
2023-06-10 16:20:57 +0900

    진료 순서 정하기

--- Programmers/Solution_143.java
+++ Programmers/Solution_143.java
@@ -6,17 +6,22 @@ import java.util.Arrays;
 public class Solution_143 {
 
 	public int[] solution(int[] param) {
+		
 		int[] cpParam = param.clone();
 		int[] result = new int[param.length];
 		Arrays.sort(cpParam);
 
 		for (int i = 0; i < param.length; i++) {
+			
 			for (int j = 0; j < param.length; j++) {
+				
 				if (param[i] == cpParam[param.length - j - 1]) {
 					result[i] = j + 1;
 					break;
 				}
+				
 			}
+			
 		}
 
 		return result;
