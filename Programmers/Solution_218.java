8f9b270782442615fdb15845f51f3ac0bb583963
yoon92kr <yoon92_kr@naver.com>
2023-06-18 19:55:58 +0900

    문자열 곱하기

---
+++ Programmers/Solution_218.java
@@ -0,0 +1,13 @@
+// 문제 : 문자열 곱하기
+// 결과 : 성공
+// Score : 100
+
+public class Solution_218 {
+  
+    public String solution(String param, int repeat) {
+
+        return param.repeat(repeat);
+       
+    }
+    
+}
