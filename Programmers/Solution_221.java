11b6aaf4886a002e81d8fbde77b1f2e127264718
yoon92kr <yoon92_kr@naver.com>
2023-06-23 08:29:55 +0900

    주사위 게임 2

--- Programmers/Solution_221.java
+++ Programmers/Solution_221.java
@@ -19,3 +19,33 @@ public class Solution_221 {
     }
     
 }
+
+
+// eriol, NOAH
+/*
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer = 1;
+
+        int count = 1;
+        if(a == b || a == c || b == c) {
+            count++;
+        }
+
+        if(a == b && b == c) {
+            count++;
+        }
+
+        for(int i = 1; i <= count; i++) {
+            answer *= (pow(a,i)+pow(b,i)+pow(c,i));
+        }
+
+        return answer;
+    }
+
+    private int pow(int a, int b) {
+        if(b == 0) return 1;
+        return a * pow(a, b-1);
+    }
+}
+ */
