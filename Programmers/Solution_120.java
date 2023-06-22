e1d66a4ed9c396c87b58e6acba22920cb47da9ce
yoon92kr <yoon92_kr@naver.com>
2023-06-22 20:56:41 +0900

    외계행성의 나이

--- Programmers/Solution_120.java
+++ Programmers/Solution_120.java
@@ -4,12 +4,17 @@
 public class Solution_120 {
 
 	public String solution(int param) {
+		
 		String strParam = Integer.toString(param);
 		String result = "";
 		for (int i = 0; i < strParam.length(); i++) {
+			
 			result += (char) (strParam.charAt(i) + 49);
+			
 		}
+		
 		return result;
+		
 	}
 
 }
