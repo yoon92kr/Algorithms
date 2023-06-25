d259ebecb55f96ffcd8571796edb1f53bb662335
yoon92kr <yoon92_kr@naver.com>
2023-06-25 22:15:07 +0900

    평행

--- Programmers/Solution_166.java
+++ Programmers/Solution_166.java
@@ -3,7 +3,9 @@
 // Score : 100
 public class Solution_166 {
 	public int solution(int[][] param) {
+		
 		int[][] pattern = new int[][] { { 0, 0 }, { 2, 3 }, { 1, 3 }, { 1, 2 } };
+		
 		for (int i = 1; i < 4; i++) {
 			double test1 = (double) (param[0][1] - param[i][1]) / (param[0][0] - param[i][0]);
 			double test2 = (double) (param[pattern[i][0]][1] - param[pattern[i][1]][1])
@@ -13,5 +15,6 @@ public class Solution_166 {
 			}
 		}
 		return 0;
+		
 	}
 }
