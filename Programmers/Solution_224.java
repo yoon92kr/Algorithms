619c9d8a1a126891b1923a2eae5d59e3e758cd1f
yoon92kr <yoon92_kr@naver.com>
2023-06-27 09:04:52 +0900

    개인정보 수집 유효기간

---
+++ Programmers/Solution_224.java
@@ -0,0 +1,49 @@
+// 문제 : 개인정보 수집 유효기간
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution_224 {
+
+    private final int MONTH = 28;
+    private final int YEAR = 336;
+
+    public int[] solution(String today, String[] terms, String[] privacies) {
+
+        HashMap<String, Integer> rule = parseMap(terms, terms.length);
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        String[] tmpValue;
+        int anchor = dateValue(today);
+        int runCnt = privacies.length;
+
+        for (int i = 0; i < runCnt; i++) {
+            tmpValue = privacies[i].split(" ");
+            if (dateValue(tmpValue[0]) + rule.get(tmpValue[1]) <= anchor) {
+                result.add(i + 1);
+            }
+        }
+
+        return result.stream().mapToInt(i -> i).toArray();
+
+    }
+
+    public HashMap<String, Integer> parseMap(String[] terms, int runCnt) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        String[] tmpValue;
+
+        for (int i = 0; i < runCnt; i++) {
+            tmpValue = terms[i].split(" ");
+            map.put(tmpValue[0], Integer.valueOf(tmpValue[1]) * MONTH);
+        }
+
+        return map;
+    }
+
+    private int dateValue(String date) {
+        int[] tmpValue = Arrays.stream(date.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        return tmpValue[0] * YEAR + tmpValue[1] * MONTH + tmpValue[2];
+
+    }
+
+}
