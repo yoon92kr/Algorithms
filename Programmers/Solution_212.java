0e196c1e0c079558c1e54e88c15988fa7b7d2c7d
yoon92kr <yoon92_kr@naver.com>
2023-07-01 20:48:46 +0900

    홀짝 구분하기

--- Programmers/Solution_212.java
+++ Programmers/Solution_212.java
@@ -8,7 +8,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Solution_212 {
+  
   public static void main(String[] args) throws Exception {
+    
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
@@ -19,5 +21,7 @@ public class Solution_212 {
     bw.flush();
     bw.close();
     br.close();
+    
   }
+  
 }
