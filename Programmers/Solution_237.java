a69a7aa132f647e3afbc80930b2d73a80586dd55
yoon92kr <yoon92_kr@naver.com>
2023-07-18 09:01:16 +0900

    공원 산책

--- Programmers/Solution_237.java
+++ Programmers/Solution_237.java
@@ -2,13 +2,13 @@
 
 class Solution_237 {
 
-    private String[][] newPark;
+    private String[] verticalMap;
     private int[] result = new int[2];
     private int[] maxDistance = new int[2];
 
-    public int[] solution(String[] park, String[] routes) {
+    public int[] solution(String[] horizontalMap, String[] routes) {
 
-        newPark = makePark(park, park[0].length(), park.length);
+        verticalMap = makePark(horizontalMap, horizontalMap[0].length(), horizontalMap.length);
 
         for (int i = 0; i < routes.length; i++) {
 
@@ -18,38 +18,40 @@ class Solution_237 {
 
     }
 
-    private String[][] makePark(String[] park, int width, int height) {
-        String[][] newPark = new String[width][height];
-        maxDistance[0] = height;
-        maxDistance[1] = width;
+    private String[] makePark(String[] park, int width, int height) {
+        String[] verticalMap = new String[width];
+        String newMap;
+        maxDistance[0] = height - 1;
+        maxDistance[1] = width - 1;
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        for (int i = 0; i < width; i++) {
+            newMap = "";
+            for (int j = 0; j < height; j++) {
                 if (park[i].charAt(j) == 'S') {
                     result[0] = i;
                     result[1] = j;
                 }
-
-                newPark[i][j] = Character.toString(park[i].charAt(j));
+                newMap += park[j].charAt(i);
             }
+            verticalMap[i] = newMap;
         }
-
-        return newPark;
+        return verticalMap;
     }
 
     // param[0] : direction
     // param[1] : distance
-    private boolean canMove(String[] param) {
+    private void move(String[] param) {
         int expLct;
         // 공원을 벗어나는지 유무를 확인한다.
-        if (param[0].equals("E") || param[0].equals("W")) {
-
-        } else {
-
+        switch (param[0]) {
+            case "E":
+            case "W":
+            case "N":
+            case "S":
         }
+
         // 경로에 장애물이 포함되어있는지 유무를 확인한다.
 
-        return true;
     }
 
 }
