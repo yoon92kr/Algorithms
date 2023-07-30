0a9496e4540bfb8f05b2580da25390d1a2e6b889
yoon92kr <yoon92_kr@naver.com>
2023-07-30 22:42:58 +0900

    수 조작하기 1

--- Programmers/Solution_247.java
+++ Programmers/Solution_247.java
@@ -1,10 +1,32 @@
 // 문제 : 수 조작하기 1
 
-class Solution_246 {
+class Solution_247 {
 
     public int solution(int n, String control) {
-        int answer = 0;
-        return answer;
+        int runCnt = control.length();
+        int result = 0;
+        String[] controlArr = control.split("");
+
+        for (int i = 0; i < runCnt; i++) {
+            result += move(controlArr[i]);
+        }
+
+        return result;
+    }
+
+    private int move(String command) {
+        switch (command) {
+            case "w":
+                return 1;
+            case "s":
+                return -1;
+            case "d":
+                return 10;
+            case "n":
+                return -10;
+        }
+
+        return 0;
     }
 
 }
