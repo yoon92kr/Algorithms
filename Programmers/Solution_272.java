d7f18bb4e2f0f4fbf7ad80b8e33a7c789beae55b
yoon92kr <yoon92_kr@naver.com>
2023-08-18 08:40:49 +0900

    할 일 목록

---
+++ Programmers/Solution_272.java
@@ -0,0 +1,16 @@
+// 문제 : 할 일 목록
+
+import java.util.stream.IntStream;
+
+class Solution_271 {
+
+    public String[] solution(String[] todo_list, boolean[] finished) {
+
+        // 완료하지 못한 일의 index 추출
+        IntStream is = IntStream.range(0, todo_list.length).filter(i -> finished[i] == false);
+        // index에 해당하는 값을 배열로 Return
+        return is.mapToObj(i -> todo_list[i]).toArray(String[]::new);
+
+    }
+
+}
