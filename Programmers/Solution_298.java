1ea311a40a2163a08af07d09acc8f29da046b4f9
yoon92kr <yoon92_kr@naver.com>
2023-09-12 08:36:47 +0900

    세 개의 구분자

--- Programmers/Solution_298.java
+++ Programmers/Solution_298.java
@@ -2,12 +2,12 @@
 
 import java.util.Arrays;
 
-class Solution_297 {
+class Solution_298 {
 
     public String[] solution(String myStr) {
 
-        String[] splitABC = String.join("", String.join("", myStr.split("a")).split("b")).split("c");
-        String[] resultArr = Arrays.stream(splitABC).filter(i -> i.length() > 0).toArray(String[]::new);
+        String[] splitABC = myStr.split("[abc]");
+        String[] resultArr = Arrays.stream(splitABC).filter(str -> str.length() > 0).toArray(String[]::new);
 
         return resultArr.length > 0 ? resultArr : new String[] { "EMPTY" };
 
