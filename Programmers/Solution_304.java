c3d21a43efa6354b6e8ebab8d9aa2556e2215fd5
yoon92kr <yoon92_kr@naver.com>
2023-09-21 09:03:32 +0900

    문자열 묶기

---
+++ Programmers/Solution_304.java
@@ -0,0 +1,20 @@
+// 문제 : 문자열 묶기
+
+import java.util.Arrays;
+
+class Solution_303 {
+
+    public int solution(String[] strArr) {
+
+        int runCnt = strArr.length;
+        int[] lenArr = new int[30]; // 문자열 길이를 index로 활용하는 배열 생성
+
+        for (int i = 0; i < runCnt; i++) {
+            lenArr[strArr[i].length() - 1]++; // 문자열 길이 index에 해당하는 값을 1 증감
+        }
+
+        return Arrays.stream(lenArr).max().getAsInt();
+
+    }
+
+}
