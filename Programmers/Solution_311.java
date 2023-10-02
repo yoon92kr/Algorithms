ddf4c187e377930b29802d33c451230d37b8b63e
yoon92kr <yoon92_kr@naver.com>
2023-10-03 04:59:34 +0900

    왼쪽 오른쪽

--- Programmers/Solution_311.java
+++ Programmers/Solution_311.java
@@ -9,11 +9,11 @@ class Solution_311 {
         int lIdx = str.indexOf("l");
         int rIdx = str.indexOf("r");
 
-        if (lIdx == -1 || rIdx == -1) {
+        if (lIdx == -1 && rIdx == -1) {
             return new String[0];
-        } else if (lIdx < rIdx) {
+        } else if (lIdx < rIdx || rIdx == -1) {
             str = str.substring(0, lIdx);
-        } else if (lIdx > rIdx) {
+        } else if (lIdx > rIdx || lIdx == -1) {
             str = str.substring(rIdx, str.length());
         }
 
