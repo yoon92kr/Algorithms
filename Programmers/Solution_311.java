bb34420c455d85bafd03a5e4c5127bf203cfb502
yoon92kr <yoon92_kr@naver.com>
2023-10-03 09:44:08 +0900

    왼쪽 오른쪽

--- Programmers/Solution_311.java
+++ Programmers/Solution_311.java
@@ -9,15 +9,22 @@ class Solution_311 {
         int lIdx = str.indexOf("l");
         int rIdx = str.indexOf("r");
 
+        // l, r 문자가 없을 경우 빈 배열을 반환한다.
         if (lIdx == -1 && rIdx == -1) {
             return new String[0];
-        } else if (lIdx < rIdx || rIdx == -1) {
-            str = str.substring(0, lIdx);
-        } else if (lIdx > rIdx || lIdx == -1) {
-            str = str.substring(rIdx, str.length());
         }
 
-        return str.split("");
+        // l 또는 r 문자가 없을 경우(-1) 최대값(20)으로 변경한다.
+        lIdx = lIdx == -1 ? 20 : lIdx;
+        rIdx = rIdx == -1 ? 20 : rIdx;
+
+        if (lIdx < rIdx) {
+            str = str.substring(0, lIdx);
+        } else if (lIdx > rIdx) {
+            str = str.substring(rIdx + 1, str.length());
+        }
+
+        return str.length() == 0 ? new String[0] : str.split("");
 
     }
 
