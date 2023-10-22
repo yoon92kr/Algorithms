4ce8849bd6863e0434d30d4e2f84777df1d65a70
yoon92kr <yoon92_kr@naver.com>
2023-10-23 08:59:00 +0900

    연속 부분 수열 합의 개수

--- Programmers/Solution_327.java
+++ Programmers/Solution_327.java
@@ -6,15 +6,18 @@ class Solution_327 {
 
     public int solution(int[] elements) {
 
+        int sum;
         int runCnt = elements.length;
         HashSet<Integer> set = new HashSet<Integer>(); // 중복 제거를 위한 HashSet 객체 선언
 
         for (int i = 1; i < runCnt; i++) {
-            int idx = 1;
 
-            /* 길이만큼 배열을 더한다 */
             for (int j = 0; j < runCnt; j++) {
-                set.add(elements[j % runCnt]);
+                sum = 0; // 길이만큼의 배열의 합을 집계
+                for (int k = j; k < j + i; k++) {
+                    sum += elements[k % runCnt];
+                }
+                set.add(sum); // 도출한 값을 HashSet에 추가
             }
 
         }
