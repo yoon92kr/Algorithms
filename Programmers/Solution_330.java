274b57fa039fb6ae7ed3cc8798b2cda6b8f420b4
yoon92kr <yoon92_kr@naver.com>
2023-10-30 08:53:49 +0900

    전화번호 목록

--- Programmers/Solution_330.java
+++ Programmers/Solution_330.java
@@ -1,15 +1,16 @@
 // 문제 : 전화번호 목록
 
+import java.util.Arrays;
+
 class Solution_330 {
 
     public boolean solution(String[] phone_book) {
-        int runCnt = phone_book.length;
+        Arrays.sort(phone_book);
+        int runCnt = phone_book.length - 1;
 
         for (int i = 0; i < runCnt; i++) {
-            for (int j = 0; j < runCnt; j++) {
-                if (j != i && phone_book[j].startsWith(phone_book[i])) {
-                    return false;
-                }
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
             }
         }
 
