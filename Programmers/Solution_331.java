64c8b71a29d0ef5eab230adca0c609a263ff0f7a
yoon92kr <yoon92_kr@naver.com>
2023-11-01 08:36:49 +0900

    금과 은 운반하기

--- Programmers/Solution_331.java
+++ Programmers/Solution_331.java
@@ -4,28 +4,29 @@ class Solution_331 {
 
     public long solution(int goldReq, int silverReq, int[] gold, int[] silver, int[] weightCarry, int[] timeReq) {
 
-        long addTime = 0L, maxTime = (goldReq * 200000L) + (silverReq * 200000L);
+        long maxTime = (goldReq * 200000L) + (silverReq * 200000L); // 최대 소요시간 산출
+        long middleTime = 0L;
         int cityCnt = gold.length;
 
-        while (addTime + 1 < maxTime) {
-            long runTime = (maxTime + addTime) / 2L;
+        /* 이진 탐색을 통해 건설에 필요한 최소 소요시간 산출 */
+        while (middleTime + 1 < maxTime) {
+            long runTime = (maxTime + middleTime) / 2L;
             long goldCarry = 0L, silverCarry = 0L, totalCarry = 0L;
 
             for (int i = 0; i < cityCnt; i++) {
-                long cnt = runTime / (2L * timeReq[i]);
-                if (runTime % (2L * timeReq[i]) >= timeReq[i])
-                    cnt++;
+                long carryCnt = (runTime / (2L * timeReq[i])) + (runTime % (2L * timeReq[i])) / timeReq[i];
+                long carryOnce = Math.min(carryCnt * weightCarry[i], gold[i] + silver[i]);
 
-                long carry = Math.min(cnt * weightCarry[i], gold[i] + silver[i]);
-                totalCarry += carry;
-                goldCarry += Math.min(carry, gold[i]);
-                silverCarry += Math.min(carry, silver[i]);
+                totalCarry += carryOnce;
+                goldCarry += Math.min(carryOnce, gold[i]);
+                silverCarry += Math.min(carryOnce, silver[i]);
             }
 
-            if (totalCarry > goldReq + silverReq && goldCarry >= goldReq && silverCarry >= silverReq)
+            if (totalCarry >= goldReq + silverReq && goldCarry >= goldReq && silverCarry >= silverReq) {
                 maxTime = runTime;
-            else
-                addTime = runTime;
+            } else {
+                middleTime = runTime;
+            }
         }
 
         return maxTime;
