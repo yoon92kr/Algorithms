f1ec9c033d509fecf30b77af529c4ba26d5d7f43
yoon92kr <yoon92_kr@naver.com>
2023-11-03 14:01:08 +0900

    튜플

--- Programmers/Solution_332.java
+++ Programmers/Solution_332.java
@@ -1,37 +1,32 @@
 // 문제 : 튜플
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution_332 {
 
     public int[] solution(String strTuple) {
 
-        String test1 = strTuple.replaceAll("\\},\\{", "|");
-        String test2 = test1.replaceAll("[\\{\\}]", "");
-        String[] strArr = test2.split("\\|"); // 배열 형태로 치환
+        String[] strArr = strTuple.replaceAll("\\},\\{", "|").replaceAll("[\\{\\}]", "").split("\\|"); // 배열 형태로 치환
         Arrays.sort(strArr, (String str1, String str2) -> str1.length() - str2.length());// 길이순 정렬
 
-        int[] result = new int[strArr.length]; // 최대 길이의 배열 크기만큼의 정수 배열 생성
-        result[0] = Integer.valueOf(strArr[0]);
+        ArrayList<String> list = new ArrayList<String>();
 
-        for (int i = 1; i < strArr.length; i++) {
+        for (int i = 0; i < strArr.length; i++) {
             String[] val = strArr[i].split(",");
 
-            if (Integer.valueOf(val[i - 1]) != result[i - 1]) {
-                result[i] = Integer.valueOf(val[i - 1]);
-            } else {
-                result[i] = Integer.valueOf(val[i]);
+            for (int j = 0; j < val.length; j++) {
+                /* list에 포함되지 않은 원소가 나타날 경우 추가한다 */
+                if (!list.contains(val[j])) {
+                    list.add(val[j]);
+                    break;
+                }
             }
         }
 
-        return result;
+        /* int 배열로 변환 후 반환 */
+        return list.stream().mapToInt(Integer::valueOf).toArray();
 
-    }
-
-    public static void main(String[] args) {
-        Solution_332 test = new Solution_332();
-
-        test.solution("{{2},{2,1},{2,1,3},{2,1,3,4}}");
     }
 
 }
