980985773a0056848196bce084ffd4275f82a864
yoon92kr <yoon92_kr@naver.com>
2023-11-13 20:15:45 +0900

    주식가격

--- Programmers/Solution_336.java
+++ Programmers/Solution_336.java
@@ -1,28 +1,23 @@
 // 문제 : 주식가격
 
-import java.util.HashMap;
-
 class Solution_336 {
 
     public int[] solution(int[] prices) {
 
-        int time = 1;
-        int len = prices.length;
-        HashMap<Integer, Integer> timeMap = new HashMap<Integer, Integer>();
-        int[] result = new int[len];
+        int len = prices.length - 1;
+        int[] result = new int[len + 1];
 
-        for (int i = len - 2; i >= 0; i--) {
+        for (int i = 0; i < len; i++) {
+            int time = 1;
 
-            int downTime = 0;
-
-            // 이후에 올 가격들 중 낮은 가격이 있을 경우 떨어진 시간에 추가한다
-            for (Integer key : timeMap.keySet()) {
-                downTime += key < prices[i] ? timeMap.get(key) : 0;
+            for (int j = i + 1; j < len; j++) {
+                if (prices[i] > prices[j]) {
+                    break;
+                }
+                time++;
             }
 
-            result[i] = time++ - downTime; // 가격이 유지된 시간을 구한 뒤 지난 시간 1초 증가
-            timeMap.put(prices[i], timeMap.getOrDefault(prices[i], 0) + 1);
-
+            result[i] = time;
         }
 
         return result;
