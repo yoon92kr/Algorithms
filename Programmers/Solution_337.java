794f4e885a45e6137509c9010d60c8c594c358df
yoon92kr <yoon92_kr@naver.com>
2023-11-15 21:57:02 +0900

    에어컨

---
+++ Programmers/Solution_337.java
@@ -0,0 +1,20 @@
+// 문제 : [1차] 뉴스 클러스터링
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class Solution_337 {
+
+    public int solution(String str1, String str2) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        String myDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(1344320225000L));
+        System.out.println(myDate);
+
+    }
+
+}
