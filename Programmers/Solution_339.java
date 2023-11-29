14b07fe581545ce1b7dd6896d0ee9455176224c0
yoon92kr <yoon92_kr@naver.com>
2023-11-30 08:54:43 +0900

    [PCCE 기출문제] 1번 / 출력

---
+++ Programmers/Solution_339.java
@@ -0,0 +1,15 @@
+// 문제 : [PCCE 기출문제] 1번 / 출력
+
+class Solution_339 {
+
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+
+}
