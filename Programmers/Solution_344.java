786b21e490c15045dff3c67d913a0940a713acd1
yoon92kr <yoon92_kr@naver.com>
2023-12-18 08:52:29 +0900

    [PCCE 기출문제] 3번 / 나이 계산

---
+++ Programmers/Solution_344.java
@@ -0,0 +1,26 @@
+// 문제 : [PCCE 기출문제] 3번 / 나이 계산
+
+import java.util.Scanner;
+
+class Solution_344 {
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int year = sc.nextInt();
+            String age_type = sc.next();
+            int answer = 0;
+
+            if (age_type.equals(
+                    "Korea")) {
+                answer = 2030 - year + 1;
+            } else if (age_type.equals("Year")) {
+
+                answer = 2030 - year;
+            }
+
+            System.out.println(answer);
+        }
+    }
+
+}
