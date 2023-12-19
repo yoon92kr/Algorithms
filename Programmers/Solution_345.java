0d684e194f726ae6df97de86ac9f604259ecd609
yoon92kr <yoon92_kr@naver.com>
2023-12-20 08:51:58 +0900

    [PCCE 기출문제] 4번 / 저축

---
+++ Programmers/Solution_345.java
@@ -0,0 +1,30 @@
+// 문제 : [PCCE 기출문제] 4번 / 저축
+
+import java.util.Scanner;
+
+class Solution_344 {
+
+    public class Solution {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int start = sc.nextInt();
+            int before = sc.nextInt();
+            int after = sc.nextInt();
+
+            int money = start;
+            int month = 1;
+            while (money < 70) {
+                money += before;
+                month++;
+            }
+            while (money < 100) {
+
+                money += after;
+                month++;
+            }
+
+            System.out.println(month);
+        }
+    }
+
+}
